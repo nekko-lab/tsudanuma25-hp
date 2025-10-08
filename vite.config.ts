1282cdbae304ab8a54169db2362882433646d083
miranon <uguisu138@gmail.com>
2025-10-08 15:34:10 +0000

    devcontainerとviteの設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // または true
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      clientPort: 5173
+    }
+  }
 })
